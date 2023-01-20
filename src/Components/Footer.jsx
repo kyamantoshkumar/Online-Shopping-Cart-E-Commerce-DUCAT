250ce8cdaef645a7e4722989001a0bbd16387b45
Mantosh <kyamantoshkumar@gmail.com>
2023-01-20 12:58:51 +0530

    Create Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='background text-center text-light p-3 mt-2'>
+        copyright@fashiomart.com
+    </div>
+  )
+}
